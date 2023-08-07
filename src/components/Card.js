a5055dbce3540584ae1fb2f9580e12e08946060f
mina-jey <mina.jeybar@gmail.com>
2023-08-07 14:08:11 +0330

    replacing unwanted changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = ({id, name, email}) => {
   return (
-    <div className=' bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5'>
-      <img alt='robots' width="200px" height="200px" src={`https://avatars.dicebear.com/api/micah/:${name}.svg`} />
+    <div className=' bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
       <div>
         <h2 className='f5'>{ name }</h2>
         <p>{ email }</p>
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -6,7 +6,7 @@ const SearchBox = ({ searchChange }) => {
       <input 
         className="pa3 ba b--green bg-lightest-blue"
         type='search' 
-        placeholder='search by name' 
+        placeholder='search robot' 
         onChange={ searchChange }
       />
     </div>
